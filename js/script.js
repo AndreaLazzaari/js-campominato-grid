0e5092584501597656363d4d642da0a92c3204a8
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-21 15:38:28 +0100

    sistemato index.html e style.css con aggiunta di un header e main con relativi tag nel css + creato in script.js una funzione e un addeventlistener

--- js/script.js
+++ js/script.js
@@ -3,3 +3,40 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata 
 // si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+// generare funzione che crea un quadrato
+// al click del bottone creare una tabella 10x10
+// inserire in ogni cella un numero progressivo da 1 a 100
+
+let buttonElement = document.querySelector ('button');
+let mainContainerElement = document.querySelector('div.main-container')
+
+buttonElement.addEventListener('click', function(){
+
+  for (let i = 0; i < 100; i++) {
+    mainContainerElement.appendChild(creazioneQuadrato(i));
+    
+  }
+
+
+
+})
+
+
+
+
+
+
+
+// FUNZIONI
+
+/**funzione che vuole andare a creare un quadrato
+ * elemento div a cui è stato assegnato una classe quadrato
+ * @returns elemento singolo quadrato
+ */
+function creazioneQuadrato() {
+  let elementoDiv = document.createElement('div');
+  elementoDiv.classList.add('quadrato');
+  
+  return elementoDiv;
+}
